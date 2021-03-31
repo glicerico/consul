383deed5571b088bfe23798e3b6daf02f1b17306
glicerico <suarezandres@gmail.com>
2021-03-30 23:01:28 -0600

    Remove all languages but spanish

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,19 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :es
+    available_locales = [
+      "es"
+    ]
+    config.i18n.fallbacks = {
+      "ca"    => "es",
+      "es-PE" => "es",
+      "eu"    => "es",
+      "fr"    => "es",
+      "gl"    => "es",
+      "it"    => "es",
+      "oc"    => "es",
+      "pt-BR" => "es",
+      "val"   => "es"
+    }
   end
 end
